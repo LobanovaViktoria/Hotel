187830ea173d0595c2c8e41554a52860c797596e
Lobanova Viktoria <va_klevtsova@mail.ru>
2023-12-17 14:04:25 +0300

    init project

---
+++ Hotel/HotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelApp.swift
+//  Hotel
+//
+//  Created by Viktoria Lobanova on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
